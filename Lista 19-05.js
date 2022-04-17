9ee1eaf77dad2f376f782caaa49cc9434acd2666
André Bastilhos <50469267+andreeeeeeeee@users.noreply.github.com>
2022-04-17 12:11:30 -0300

    Update Lista 19-05.js

--- Lista 19-05.js
+++ Lista 19-05.js
@@ -5,14 +5,20 @@ Indique se um aluno passou ou rodou. Para isso leia suas notas e sua frequência
 
 var bts=prompt("Em sua escola, a avaliação é feita em bimestres, trimestres ou semestres?");
 var pf=prompt("Em sua escola, a frequência pode interferir no resultado final do ano?");
+var pn=Number(prompt("De quanta nota você precisa para passar?"));
+var pf2, pf, n3, n4;
+if (pf == "sim") {
+	pf2=Number(prompt("De quanta frequência você precisa para passar?"));
+	f=Number(prompt("Digite sua frequência:"));
+}
+var n1=Number(prompt("Digite a sua primeira nota:"));
+var n2=Number(prompt("Digite a sua segunda nota:"));
+if (bts!="semestres") {
+	n3=Number(prompt("Digite a sua terceira nota:"));
+} else if (bts=="bimestres") {
+	n4=Number(prompt("Digite a sua quarta nota:"));
+}
 if (bts=="bimestres" && pf=="sim") {
-	var pf2=Number(prompt("De quanta frequência você precisa para passar?"));
-	var pn=Number(prompt("De quanta nota você precisa para passar?"));
-	var n1=Number(prompt("Digite a sua primeira nota:"));
-	var n2=Number(prompt("Digite a sua segunda nota:"));
-	var n3=Number(prompt("Digite a sua terceira nota:"));
-	var n4=Number(prompt("Digite a sua quarta nota:"));
-	var f=Number(prompt("Digite sua frequência:"));
 	var mf=(n1+n2+n3+n4)/4
 	if (f>=pf2 && mf>=pn) {
 		alert("Você passou!")
@@ -25,11 +31,6 @@ if (bts=="bimestres" && pf=="sim") {
 	}
 }
 if (bts=="bimestres" && pf=="não") {
-	var pn=Number(prompt("De quanta nota você precisa para passar?"));
-	var n1=Number(prompt("Digite a sua primeira nota:"));
-	var n2=Number(prompt("Digite a sua segunda nota:"));
-	var n3=Number(prompt("Digite a sua terceira nota:"));
-	var n4=Number(prompt("Digite a sua quarta nota:"));
 	var mf=(n1+n2+n3+n4)/4
 	if (mf>=pn) {
 		alert("Você passou!")
@@ -38,12 +39,6 @@ if (bts=="bimestres" && pf=="não") {
 	}
 }
 if (bts=="trimestres" && pf=="sim") {
-	var pf2=Number(prompt("De quanta frequência você precisa para passar?"));
-	var pn=Number(prompt("De quanta nota você precisa para passar?"));
-	var n1=Number(prompt("Digite a sua primeira nota:"));
-	var n2=Number(prompt("Digite a sua segunda nota:"));
-	var n3=Number(prompt("Digite a sua terceira nota:"));
-	var f=Number(prompt("Digite sua frequência:"));
 	var mf=(n1+n2+n3)/3
 	if (f>=pf2 && mf>=pn) {
 		alert("Você passou!")
@@ -56,10 +51,6 @@ if (bts=="trimestres" && pf=="sim") {
 	}
 }
 if (bts=="trimestres" && pf=="não") {
-	var pn=Number(prompt("De quanta nota você precisa para passar?"));
-	var n1=Number(prompt("Digite a sua primeira nota:"));
-	var n2=Number(prompt("Digite a sua segunda nota:"));
-	var n3=Number(prompt("Digite a sua terceira nota:"));
 	var mf=(n1+n2+n3)/3
 	if (mf>=pn) {
 		alert("Você passou!")
@@ -68,11 +59,6 @@ if (bts=="trimestres" && pf=="não") {
 	}
 }
 if (bts=="semestres" && pf=="sim") {
-	var pf2=Number(prompt("De quanta frequência você precisa para passar?"));
-	var pn=Number(prompt("De quanta nota você precisa para passar?"));
-	var n1=Number(prompt("Digite a sua primeira nota:"));
-	var n2=Number(prompt("Digite a sua segunda nota:"));
-	var f=Number(prompt("Digite sua frequência:"));
 	var mf=(n1+n2)/2
 	if (f>=pf2 && mf>=pn) {
 		alert("Você passou!")
@@ -85,10 +71,6 @@ if (bts=="semestres" && pf=="sim") {
 	}
 }
 if (bts=="semestres" && pf=="não") {
-	var pn=Number(prompt("De quanta nota você precisa para passar?"));
-	var n1=Number(prompt("Digite a sua primeira nota:"));
-	var n2=Number(prompt("Digite a sua segunda nota:"));
-	var f=Number(prompt("Digite sua frequência:"));
 	var mf=(n1+n2)/2
 	if (f>=pf2 && mf>=pn) {
 		alert("Você passou!")
@@ -100,6 +82,7 @@ if (bts=="semestres" && pf=="não") {
 		alert("Você rodou!")
 	}
 }
+if (bts=="bimestres" && pf=="sim") {
 	var mf=(n1+n2+n3+n4)/4
 	if (f>=pf2 && mf>=pn) {
 		alert("Você passou!")
@@ -112,11 +95,6 @@ if (bts=="semestres" && pf=="não") {
 	}
 }
 if (bts=="bimestres" && pf=="não") {
-	var pn=Number(prompt("De quanta nota você precisa para passar?"));
-	var n1=Number(prompt("Digite a sua primeira nota:"));
-	var n2=Number(prompt("Digite a sua segunda nota:"));
-	var n3=Number(prompt("Digite a sua terceira nota:"));
-	var n4=Number(prompt("Digite a sua quarta nota:"));
 	var mf=(n1+n2+n3+n4)/4
 	if (mf>=pn) {
 		alert("Você passou!")
@@ -125,12 +103,6 @@ if (bts=="bimestres" && pf=="não") {
 	}
 }
 if (bts=="trimestres" && pf=="sim") {
-	var pf2=Number(prompt("De quanta frequência você precisa para passar?"));
-	var pn=Number(prompt("De quanta nota você precisa para passar?"));
-	var n1=Number(prompt("Digite a sua primeira nota:"));
-	var n2=Number(prompt("Digite a sua segunda nota:"));
-	var n3=Number(prompt("Digite a sua terceira nota:"));
-	var f=Number(prompt("Digite sua frequência:"));
 	var mf=(n1+n2+n3)/3
 	if (f>=pf2 && mf>=pn) {
 		alert("Você passou!")
@@ -143,10 +115,6 @@ if (bts=="trimestres" && pf=="sim") {
 	}
 }
 if (bts=="trimestres" && pf=="não") {
-	var pn=Number(prompt("De quanta nota você precisa para passar?"));
-	var n1=Number(prompt("Digite a sua primeira nota:"));
-	var n2=Number(prompt("Digite a sua segunda nota:"));
-	var n3=Number(prompt("Digite a sua terceira nota:"));
 	var mf=(n1+n2+n3)/3
 	if (mf>=pn) {
 		alert("Você passou!")
@@ -155,11 +123,6 @@ if (bts=="trimestres" && pf=="não") {
 	}
 }
 if (bts=="semestres" && pf=="sim") {
-	var pf2=Number(prompt("De quanta frequência você precisa para passar?"));
-	var pn=Number(prompt("De quanta nota você precisa para passar?"));
-	var n1=Number(prompt("Digite a sua primeira nota:"));
-	var n2=Number(prompt("Digite a sua segunda nota:"));
-	var f=Number(prompt("Digite sua frequência:"));
 	var mf=(n1+n2)/2
 	if (f>=pf2 && mf>=pn) {
 		alert("Você passou!")
@@ -172,18 +135,10 @@ if (bts=="semestres" && pf=="sim") {
 	}
 }
 if (bts=="semestres" && pf=="não") {
-	var pn=Number(prompt("De quanta nota você precisa para passar?"));
-	var n1=Number(prompt("Digite a sua primeira nota:"));
-	var n2=Number(prompt("Digite a sua segunda nota:"));
-	var f=Number(prompt("Digite sua frequência:"));
 	var mf=(n1+n2)/2
-	if (f>=pf2 && mf>=pn) {
+	if (mf>=pn) {
 		alert("Você passou!")
-	} else if(f>=pf2 && mf<pn) {
-		alert("Você rodou por causa da média!")
-	} else if (f<pf2 && mf>=pn) {
-		alert("Você rodou por causa da frequência!")
-	} else if (f<pf2 && mf<pn) {
+	} else if (mf<pn) {
 		alert("Você rodou!")
 	}
 }
