be6b947f161e419e6e773ea6f0e0aed83d5e3add
André Bastilhos <50469267+andreeeeeeeee@users.noreply.github.com>
2022-04-17 12:15:31 -0300

    Update Lista 19-05.js

--- Lista 19-05.js
+++ Lista 19-05.js
@@ -6,7 +6,7 @@ Indique se um aluno passou ou rodou. Para isso leia suas notas e sua frequência
 var bts=prompt("Em sua escola, a avaliação é feita em bimestres, trimestres ou semestres?");
 var pf=prompt("Em sua escola, a frequência pode interferir no resultado final do ano?");
 var pn=Number(prompt("De quanta nota você precisa para passar?"));
-var pf2, pf, n3, n4;
+var pf2 = 0, pf = 0, n3, n4, mf;
 if (pf == "sim") {
 	pf2=Number(prompt("De quanta frequência você precisa para passar?"));
 	f=Number(prompt("Digite sua frequência:"));
@@ -15,130 +15,22 @@ var n1=Number(prompt("Digite a sua primeira nota:"));
 var n2=Number(prompt("Digite a sua segunda nota:"));
 if (bts!="semestres") {
 	n3=Number(prompt("Digite a sua terceira nota:"));
-} else if (bts=="bimestres") {
+} if (bts=="bimestres") {
 	n4=Number(prompt("Digite a sua quarta nota:"));
 }
-if (bts=="bimestres" && pf=="sim") {
-	var mf=(n1+n2+n3+n4)/4
-	if (f>=pf2 && mf>=pn) {
-		alert("Você passou!")
-	} else if(f>=pf2 && mf<pn) {
-		alert("Você rodou por causa da média!")
-	} else if (f<pf2 && mf>=pn) {
-		alert("Você rodou por causa da frequência!")
-	} else if (f<pf2 && mf<pn) {
-		alert("Você rodou!")
-	}
+if (bts=="bimestres") {
+	mf=(n1+n2+n3+n4)/4
+} else if (bts=="trimestres") {
+	mf=(n1+n2+n3)/3
+} else if (bts=="semestres") {
+	mf=(n1+n2)/2
 }
-if (bts=="bimestres" && pf=="não") {
-	var mf=(n1+n2+n3+n4)/4
-	if (mf>=pn) {
-		alert("Você passou!")
-	} else {
-		alert("Você rodou!")
-	}
-}
-if (bts=="trimestres" && pf=="sim") {
-	var mf=(n1+n2+n3)/3
-	if (f>=pf2 && mf>=pn) {
-		alert("Você passou!")
-	} else if(f>=pf2 && mf<pn) {
-		alert("Você rodou por causa da média!")
-	} else if (f<pf2 && mf>=pn) {
-		alert("Você rodou por causa da frequência!")
-	} else if (f<pf2 && mf<pn) {
-		alert("Você rodou!")
-	}
-}
-if (bts=="trimestres" && pf=="não") {
-	var mf=(n1+n2+n3)/3
-	if (mf>=pn) {
-		alert("Você passou!")
-	} else {
-		alert("Você rodou!")
-	}
-}
-if (bts=="semestres" && pf=="sim") {
-	var mf=(n1+n2)/2
-	if (f>=pf2 && mf>=pn) {
-		alert("Você passou!")
-	} else if(f>=pf2 && mf<pn) {
-		alert("Você rodou por causa da média!")
-	} else if (f<pf2 && mf>=pn) {
-		alert("Você rodou por causa da frequência!")
-	} else if (f<pf2 && mf<pn) {
-		alert("Você rodou!")
-	}
-}
-if (bts=="semestres" && pf=="não") {
-	var mf=(n1+n2)/2
-	if (f>=pf2 && mf>=pn) {
-		alert("Você passou!")
-	} else if(f>=pf2 && mf<pn) {
-		alert("Você rodou por causa da média!")
-	} else if (f<pf2 && mf>=pn) {
-		alert("Você rodou por causa da frequência!")
-	} else if (f<pf2 && mf<pn) {
-		alert("Você rodou!")
-	}
-}
-if (bts=="bimestres" && pf=="sim") {
-	var mf=(n1+n2+n3+n4)/4
-	if (f>=pf2 && mf>=pn) {
-		alert("Você passou!")
-	} else if(f>=pf2 && mf<pn) {
-		alert("Você rodou por causa da média!")
-	} else if (f<pf2 && mf>=pn) {
-		alert("Você rodou por causa da frequência!")
-	} else if (f<pf2 && mf<pn) {
-		alert("Você rodou!")
-	}
-}
-if (bts=="bimestres" && pf=="não") {
-	var mf=(n1+n2+n3+n4)/4
-	if (mf>=pn) {
-		alert("Você passou!")
-	} else {
-		alert("Você rodou!")
-	}
-}
-if (bts=="trimestres" && pf=="sim") {
-	var mf=(n1+n2+n3)/3
-	if (f>=pf2 && mf>=pn) {
-		alert("Você passou!")
-	} else if(f>=pf2 && mf<pn) {
-		alert("Você rodou por causa da média!")
-	} else if (f<pf2 && mf>=pn) {
-		alert("Você rodou por causa da frequência!")
-	} else if (f<pf2 && mf<pn) {
-		alert("Você rodou!")
-	}
-}
-if (bts=="trimestres" && pf=="não") {
-	var mf=(n1+n2+n3)/3
-	if (mf>=pn) {
-		alert("Você passou!")
-	} else {
-		alert("Você rodou!")
-	}
-}
-if (bts=="semestres" && pf=="sim") {
-	var mf=(n1+n2)/2
-	if (f>=pf2 && mf>=pn) {
-		alert("Você passou!")
-	} else if(f>=pf2 && mf<pn) {
-		alert("Você rodou por causa da média!")
-	} else if (f<pf2 && mf>=pn) {
-		alert("Você rodou por causa da frequência!")
-	} else if (f<pf2 && mf<pn) {
-		alert("Você rodou!")
-	}
-}
-if (bts=="semestres" && pf=="não") {
-	var mf=(n1+n2)/2
-	if (mf>=pn) {
-		alert("Você passou!")
-	} else if (mf<pn) {
-		alert("Você rodou!")
-	}
+if (f>=pf2 && mf>=pn) {
+	alert("Você passou!");
+} else if(f>=pf2 && mf<pn) {
+	alert("Você rodou por causa da média!");
+} else if (f<pf2 && mf>=pn) {
+	alert("Você rodou por causa da frequência!");
+} else if (f<pf2 && mf<pn) {
+	alert("Você rodou!");
 }
